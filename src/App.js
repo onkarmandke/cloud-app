58df6c1c9bcd75ea0371c252db6ca1238486ec8e
onkarmandke <onkarmandke@gmail.com>
2023-07-15 21:34:25 +0530

    My first react app on cloud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { ChangeEvent, useState } from "React";
 
 function App() {
+  const [number1, setNumber1] = React.useState();
+  const [number2, setNumber2] = React.useState();
+  const [total, setTotal] = React.useState(0);
+
+  function calculateTotal() {
+    setTotal(number1 + number2);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Adding Two Numbers</h2>
+      <input
+        placeholder="First Number"
+        type="number"
+        value={number1}
+        onChange={(e) => setNumber1(+e.target.value)}
+      />
+      <input
+        placeholder="Second Number"
+        type="number"
+        value={number2}
+        onChange={(e) => setNumber2(+e.target.value)}
+      />
+
+      <button onClick={calculateTotal}>Add Two Numbers</button>
+      <p>Total: {total || ""}</p>
     </div>
   );
 }
 
+ReactDOM.render(<App />, document.getElementById("root"));
 export default App;
